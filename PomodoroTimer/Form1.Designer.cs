a82d35ff498c322cb9ecef6d3a71bec32d0fff74
peace098beat <peace098beat.t@gmail.com>
2018-11-20 22:05:10 +0900

    V1

--- PomodoroTimer/Form1.Designer.cs
+++ PomodoroTimer/Form1.Designer.cs
@@ -29,12 +29,86 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_lasttime = new System.Windows.Forms.Label();
+            this.label_message = new System.Windows.Forms.Label();
+            this.timer_every1s = new System.Windows.Forms.Timer(this.components);
+            this.button_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_lasttime
+            // 
+            this.label_lasttime.AutoSize = true;
+            this.label_lasttime.BackColor = System.Drawing.Color.Transparent;
+            this.label_lasttime.Font = new System.Drawing.Font("MS UI Gothic", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_lasttime.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label_lasttime.Location = new System.Drawing.Point(17, 38);
+            this.label_lasttime.Name = "label_lasttime";
+            this.label_lasttime.Size = new System.Drawing.Size(210, 80);
+            this.label_lasttime.TabIndex = 0;
+            this.label_lasttime.Text = "00:00";
+            // 
+            // label_message
+            // 
+            this.label_message.AutoSize = true;
+            this.label_message.BackColor = System.Drawing.SystemColors.Control;
+            this.label_message.Font = new System.Drawing.Font("游明朝", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_message.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label_message.Location = new System.Drawing.Point(24, 20);
+            this.label_message.Name = "label_message";
+            this.label_message.Size = new System.Drawing.Size(30, 16);
+            this.label_message.TabIndex = 1;
+            this.label_message.Text = "wait";
+            this.label_message.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer_every1s
+            // 
+            this.timer_every1s.Enabled = true;
+            this.timer_every1s.Tick += new System.EventHandler(this.timer_every1s_Tick);
+            // 
+            // button_start
+            // 
+            this.button_start.BackColor = System.Drawing.SystemColors.Control;
+            this.button_start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_start.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button_start.Location = new System.Drawing.Point(27, 39);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(204, 110);
+            this.button_start.TabIndex = 2;
+            this.button_start.Text = "START";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Visible = false;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(248, 169);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label_lasttime);
+            this.Controls.Add(this.label_message);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pomodoro Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_lasttime;
+        private System.Windows.Forms.Label label_message;
+        private System.Windows.Forms.Timer timer_every1s;
+        private System.Windows.Forms.Button button_start;
     }
 }
 
--- PomodoroTimer/Form1.cs
+++ PomodoroTimer/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,190 @@ namespace PomodoroTimer
 {
     public partial class Form1 : Form
     {
+
+        enum TimerState
+        {
+            Wait,
+            Working,
+            Relax
+        }
+        private TimerState AppState = TimerState.Wait;
+
+        private int WorkingTimeLimitSec = 10;// 25*60; //分 25;
+        private int RelaxTimerLimitSec = 10;// 5*60; //分  5;
+
+        private Stopwatch stopwatch;
+        private int LimitTime;
+        private FormWindowState preWindowState;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ストップウォッチを初期化
+            this.stopwatch = new Stopwatch();
+            this.stopwatch.Stop();
+
+            // 残り時間を初期化
+
+            this.Width = 220;
+            this.Height = 200;
+
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.WindowState = FormWindowState.Normal;
+
+        }
+
+
+        private void timer_every1s_Tick(object sender, EventArgs e)
+        {
+
+            switch (AppState)
+            {
+                case TimerState.Wait:
+                    this.button_start.Visible = true;
+                    this.button_start.Enabled = true;
+                    this.label_lasttime.Visible = false;
+                    this.LimitTime = WorkingTimeLimitSec;
+                    this.label_message.Text = "App Waiting. Please Start..";
+                    // 色
+                    Color fore_color = Color.Gray;
+                    this.label_message.BackColor = Color.Transparent;
+                    this.label_lasttime.BackColor = Color.Transparent;
+                    this.button_start.BackColor = Color.Transparent;
+                    this.label_message.ForeColor = fore_color;
+                    this.label_lasttime.ForeColor = fore_color;
+                    this.button_start.ForeColor = fore_color;
+
+                    this.BackColor = Color.Snow;
+                    this.ForeColor = fore_color;
+
+                    // 画面位置
+                    this.TopMost = false;
+
+                    if(this.WindowState == FormWindowState.Maximized) this.WindowState = FormWindowState.Normal;
+
+                    this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+
+                    break;
+
+                case TimerState.Working:
+                    this.LimitTime = WorkingTimeLimitSec;
+                    this.button_start.Visible = false;
+                    this.label_lasttime.Visible = true;
+
+                    this.label_message.Text = "Working..";
+
+                    // 色
+                    this.BackColor = Color.Black;
+                    fore_color = Color.Crimson;
+                    this.label_message.ForeColor = fore_color;
+                    this.label_lasttime.ForeColor = fore_color;
+                    this.button_start.ForeColor = fore_color;
+
+                    // 画面位置
+                    this.TopMost = false;
+                    //this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+
+                    break;
+
+                case TimerState.Relax:
+                    this.LimitTime = RelaxTimerLimitSec;
+                    this.button_start.Visible = false;
+                    this.label_lasttime.Visible = true;
+                    this.label_message.Text = "Relax";
+
+                    // 色
+                    this.BackColor = Color.LightSkyBlue;
+                    fore_color = Color.Pink;
+                    this.label_message.ForeColor = fore_color;
+                    this.label_lasttime.ForeColor = fore_color;
+                    this.button_start.ForeColor = fore_color;
+
+                    // 画面位置
+                    this.TopMost = true;
+                        this.WindowState = FormWindowState.Maximized;
+                    this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+                    break;
+
+                default:
+                    break;
+            }
+
+            // **
+            // 残り時間計測
+            // **
+            TimeSpan elapsed = this.stopwatch.Elapsed;
+            TimeSpan timeLimit = new TimeSpan(0, 0, LimitTime);
+            TimeSpan lastTime = timeLimit - elapsed;
+
+            // **
+            // 状態遷移の判定
+            // **
+            if (timeLimit.Ticks - elapsed.Ticks < 0)
+            {
+                // 残り時間が0になりました
+                switch (this.AppState)
+                {
+                    case TimerState.Wait:
+                        break;
+                    case TimerState.Working:
+                        // 最前面
+                        // 最大画面にする
+                        this.AppState = TimerState.Relax;
+                        this.stopwatch.Restart();
+                        break;
+                    case TimerState.Relax:
+                        // 画面を通常サイズに戻す
+                        // 最前面をやめる
+                        this.AppState = TimerState.Wait;
+                        this.stopwatch.Reset();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            // 経過時間を表示
+            //this.label_lasttime.Text = elapsed.ToString(@"mm\:ss");
+            // 残り時間 表示
+            this.label_lasttime.Text = lastTime.ToString(@"mm\:ss");
+
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            switch (this.AppState)
+            {
+                case TimerState.Wait:
+                    // タイマースタート
+                    this.AppState = TimerState.Working; // 状態を遷移
+                    this.stopwatch.Restart();
+                    this.button_start.Enabled = false; // ボタンを使用不可に
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+            this.label_lasttime.Margin = new Padding(0);
+            this.label_lasttime.Left = (int)Math.Ceiling((this.Width - this.label_lasttime.Width) / 2.2) ; // 2.2は経験値 2ではずれる
+            this.label_lasttime.Top = (int)Math.Ceiling((this.Height - this.label_lasttime.Height) / 2.2) ;
+
+            this.button_start.Margin = new Padding(0);
+            this.button_start.Width = (int)(this.Width * 0.7);
+            this.button_start.Height = (int)(this.Height * 0.5);
+            this.button_start.Left = (int)Math.Ceiling((this.Width - this.button_start.Width) / 2.2);
+            this.button_start.Top = (int)Math.Ceiling((this.Height - this.button_start.Height) / 2.2);
+
         }
     }
 }
